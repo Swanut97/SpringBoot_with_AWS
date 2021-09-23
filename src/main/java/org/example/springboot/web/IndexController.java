414b305e40453acc874794c88b60be6a8f062e84
BIG <zerfrawin@gmail.com>
2021-09-23 21:44:11 +0900

    chap. 4
    게시물 삭제 마무리
    스터디 단위 디버깅 및 오류 공유

--- src/main/java/org/example/springboot/web/IndexController.java
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package org.example.springboot.web;
 
 import org.example.springboot.domain.posts.PostsRepository;
+import org.example.springboot.service.posts.PostsService;
 import org.example.springboot.web.dto.PostsResponseDto;
 
 import lombok.RequiredArgsConstructor;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IndexController {
 
-    private final PostsRepository postsService;
+    private final PostsService postsService;
 
     @GetMapping("/")
     public String index(Model model) { // model: 서버 템플릿 엔진에서 사용할 수 있는 객체로 저장
@@ -30,9 +31,9 @@ public class IndexController {
 
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model) {
-        // 뭔가 문제가 있어서 dto로 할당해서 파라미터로 주기가 불가능. 캐스팅 문제로 추정
+        PostsResponseDto dto = postsService.findById(id);
 
-        model.addAttribute("post", postsService.findById(id));
+        model.addAttribute("post", dto);
 
         return "posts-update";
     }
