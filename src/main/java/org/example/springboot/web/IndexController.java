f456c5cb18a97fb7deb92c1c29219c84e34b6f92
BIG <zerfrawin@gmail.com>
2021-09-29 18:32:17 +0900

    chap. 5
    이그노어 설정 중 실수로 다른 파일 삭제한 것 복구
    Google 로그인 구현

--- src/main/java/org/example/springboot/web/IndexController.java
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package org.example.springboot.web;
 
+import org.example.springboot.config.auth.dto.SessionUser;
 import org.example.springboot.service.posts.PostsService;
 import org.example.springboot.web.dto.PostsResponseDto;
 
@@ -9,16 +10,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) { // model: 서버 템플릿 엔진에서 사용할 수 있는 객체로 저장
             // 여기서 가져온 결과를 posts로 index.mustache에 전달
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
